1ec5130fb3e712e6beb4f8998d97eac49feb76bf
Girish acharya <acgir250@gmail.com>
2020-11-07 19:43:02 +0530

    Initial commit

---
+++ src/test/java/com/omniorio/eureka/server/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omniorio.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
